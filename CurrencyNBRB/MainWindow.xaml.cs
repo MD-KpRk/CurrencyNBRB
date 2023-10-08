2ff36ee89a896873b60a13f99b20b5dbd7dbe15d
MDKPRK <griferad@gmail.com>
2023-10-08 14:44:28 +0300

    test

--- CurrencyNBRB/MainWindow.xaml.cs
+++ CurrencyNBRB/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace CurrencyNBRB
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>//
     public partial class MainWindow : Window
     {
         public MainWindow()
