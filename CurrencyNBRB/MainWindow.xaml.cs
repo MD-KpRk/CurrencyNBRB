09dcb1fe6f52684fc4d3bf833c4fcb85bcd7c70c
MD-KpRk <griferad@gmail.com>
2023-10-29 11:17:27 +0300

    Упростил backend и frondend

--- CurrencyNBRB/MainWindow.xaml.cs
+++ CurrencyNBRB/MainWindow.xaml.cs
@@ -1,23 +1,66 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace CurrencyNBRB
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        ObservableCollection<CurrencyControl> labels = new ObservableCollection<CurrencyControl>();
+        //ObservableCollection<CurrencyControl> labels = new ObservableCollection<CurrencyControl>();
+
+        string updatedatetext = string.Empty;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string UpdateDateText
+        {
+            get
+            {
+                return "Последнее обновление" + "\n" + updatedatetext;
+            }
+            set
+            {
+                updatedatetext = value;
+                OnPropertyChanged("UpdateDateText");
+            }
+        }
+        private void OnPropertyChanged(string info)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            //DataContext = this;
-            myItems.ItemsSource = labels;
+            Init();
+            Update();
+        }
+
+        public void Init()
+        {
+            DataContext = this;
+
+        }
+
+        public void Update()
+        {
+            DateTime dateTime= DateTime.Now;
+            UpdateDateText = dateTime.ToShortDateString() + " " + dateTime.ToLongTimeString();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //test
+        }
 
-            
-            labels.Add(new CurrencyControl("CurrencyName"));
+        private void Button_Click_1(object sender, RoutedEventArgs e) // Update Button
+        {
+            Update();
         }
     }
 }
