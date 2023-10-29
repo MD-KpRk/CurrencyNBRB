8564fedfc5b06dd7ae616f1279615ca1a60430a9
MD-KpRk <griferad@gmail.com>
2023-10-29 17:00:26 +0300

    Прописал моделям поля

--- CurrencyNBRB/Models/Currency.cs
+++ CurrencyNBRB/Models/Currency.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CurrencyNBRB.Models
 {
-    internal class Currency
+    public class Currency
     {
+        [Key]
+        public int Cur_ID { get; set; }
+        public int? Cur_ParentID { get; set; }
+        public string? Cur_Code { get; set; }
+        public string? Cur_Name { get; set; }
+        public string? Cur_Abbreviation { get; set; }
+        public string? Cur_QuotName { get; set; }
+        public string? Cur_NameMulti { get; set; }
+        public int Cur_Scale { get; set; }
+        public int Cur_Periodicity { get; set; }
+        public System.DateTime Cur_DateStart { get; set; }
+        public System.DateTime Cur_DateEnd { get; set; }
     }
 }
--- CurrencyNBRB/Models/RateShort.cs
+++ CurrencyNBRB/Models/RateShort.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CurrencyNBRB.Models
 {
-    internal class RateShort
+    public class RateShort
     {
+        public int Cur_ID { get; set; }
+        [Key]
+        public System.DateTime Date { get; set; }
+        public decimal? Cur_OfficialRate { get; set; }
     }
 }
